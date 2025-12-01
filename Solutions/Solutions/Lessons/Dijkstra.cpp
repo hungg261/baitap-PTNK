8686fa0bb1ba2d2f179dbd02e7cfdeb0025e02f7
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-01 10:30:11 +0700

    Dijkstra

---
+++ Solutions/Solutions/Lessons/Dijkstra.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************
+Link: Dijkstra
+Code: Dijkstra
+Time (YYYY-MM-DD-hh.mm.ss): 2025-12-01-10.26.51
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int MAXN = 2e5;
+const int INF = 1e18;
+vector<pair<int, int>> adj[MAXN + 5];
+int n, m;
+
+int dist[MAXN + 5], parent[MAXN + 5];
+void dijkstra(){
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+    pq.push({0, 1});
+
+    fill(dist + 1, dist + n + 1, INF);
+    dist[1] = 0;
+
+    while(!pq.empty()){
+        int curCost, u; tie(curCost, u) = pq.top();
+        pq.pop();
+
+        if(curCost != dist[u]) continue;
+
+        for(const auto& p: adj[u]){
+            int cost, v; tie(cost, v) = p;
+            int newCost = dist[u] + cost;
+            if(newCost < dist[v]){
+                dist[v] = newCost;
+                parent[v] = u;
+
+                pq.push({newCost, v});
+            }
+        }
+    }
+
+    if(dist[n] == INF){
+        cout << "-1\n";
+        return;
+    }
+
+    cout << dist[n] << '\n';
+
+    int cur = n;
+    vector<int> res;
+    while(cur != 0){
+        res.push_back(cur);
+        cur = parent[cur];
+    }
+
+    reverse(begin(res), end(res));
+    for(int node: res) cout << node << ' ';
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //freopen("Dijkstra.INP","r",stdin);
+    //freopen("Dijkstra.OUT","w",stdout);
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for(int i = 1; i <= m; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        adj[a].push_back({c, b});
+        adj[b].push_back({c, a});
+    }
+
+    dijkstra();
+
+    return 0;
+}
