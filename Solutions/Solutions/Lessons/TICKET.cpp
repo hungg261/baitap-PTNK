6e3add87538e7c20eae9db420048b14a7287b6e3
hungg261 <phanthanhhung.26012010@gmail.com>
2026-01-13 13:57:38 +0700

    new pset

---
+++ Solutions/Solutions/Lessons/TICKET.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+Link: TICKET
+Code: TICKET
+Time (YYYY-MM-DD-hh.mm.ss): 2026-01-13-13.52.08
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e5, MAXM = 5e5;
+int nodes[MAXN * 4 + 5], lazy[MAXN * 4 + 5];
+
+void push(int id, int l, int r){
+    int mid = (l + r) >> 1;
+    int &lz = lazy[id];
+
+    int mid = (l + r) >> 1;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //freopen("TICKET.INP","r",stdin);
+    //freopen("TICKET.OUT","w",stdout);
+
+
+    return 0;
+}
